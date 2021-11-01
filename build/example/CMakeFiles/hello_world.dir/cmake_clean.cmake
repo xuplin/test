3a50383fec0c149abf951303e3e122a836e9e5cf
xuplin <aaa20000913@gmail.com>
2021-11-01 20:21:04 +0800

    update OBS_stragety update image.cpp update image.h update stragety.cpp update stragety.h

---
+++ build/example/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/src/hello_world.cpp.o"
+  "/home/xuplin/test/devel/lib/example/hello_world.pdb"
+  "/home/xuplin/test/devel/lib/example/hello_world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
